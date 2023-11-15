22eedbd8e99bd102aaba55f82eb1ca18b9de731a
Wojtek Gawroński <wojgaw@amazon.com>
2023-11-15 11:46:23 +0100

    Improving IaC and adding AWS Lambda Powertools for TypeScript.

--- examples/02-deploying-cqrs-in-aws-lambda-environment/approach-00-monolithic-dispatcher/library/test/handler.test.ts
+++ examples/02-deploying-cqrs-in-aws-lambda-environment/approach-00-monolithic-dispatcher/library/test/handler.test.ts
@@ -1,12 +1,12 @@
 import { expect } from '@jest/globals';
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { main } from '../app/handler';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { plainHandler } from '../src/handler';
 import * as payload from '../docs/events/api-gateway.json';
 
 describe('Unit test for app handler', function () {
   it('verifies basic successful response', async () => {
-    const event = payload as unknown;
-    const result: APIGatewayProxyResult = await main(event as APIGatewayProxyEvent);
+    const event = (payload as unknown) as APIGatewayProxyEvent;
+    const result = await plainHandler(event);
 
     expect(result.statusCode).toEqual(200);
     expect(result.body).toEqual(JSON.stringify({ message: 'Hello, World!' }));
